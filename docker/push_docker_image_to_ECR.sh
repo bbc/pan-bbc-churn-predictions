258f7c735d99c820ded87f483a3d8580875019cd
teodorpopescu-bbc <teodor.popescu@bbc.co.uk>
2020-06-08 09:48:31 +0100

    changes to Makefile copy and pushing to ECR

--- docker/push_docker_image_to_ECR.sh
+++ docker/push_docker_image_to_ECR.sh
@@ -3,9 +3,9 @@
 export AWS_DEFAULT_REGION=eu-west-1
 ENV=${ENV:-int}
 
-ACCOUNT="639227811136"
+ACCOUNT="657378245742"
 IMAGE="pan-bbc-churn-predictions"
-TAG="0.0.3"
+TAG="0.0.1"
 
 ECR_PW=$(aws ecr get-login --region eu-west-1 --registry-ids ${ACCOUNT} | cut -d' ' -f6)
 docker login https://${ACCOUNT}.dkr.ecr.eu-west-1.amazonaws.com -u AWS -p $ECR_PW
