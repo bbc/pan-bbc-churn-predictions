5919409e226151a30db7c56949cd56b3e167d459
garsir01 <robert.garside@bbc.co.uk>
2020-12-16 17:30:29 +0000

    remove old src and SQL dirs

--- SQL/pipeline/unload_churn_train.sql
+++
@@ -1,9 +0,0 @@
-
-UNLOAD($$
-SELECT * FROM central_insights_sandbox.tp_churn_iplayer_training_sample
-    $$)
-TO '<params.S3_OUTPUT_PATH>' -- 's3://insights-sport/segmentation/live/uas/'
-CREDENTIALS 'aws_access_key_id=<params.AWS_ACCESS_KEY_ID>;aws_secret_access_key=<params.AWS_SECRET_ACCESS_KEY>;token=<params.TOKEN>'
-ALLOWOVERWRITE
-PARALLEL OFF
-BZIP2;
